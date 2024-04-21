be0cff410008a9dcb9bf54b36e9500431232c47b
jgding <2689871524@qq.com>
2024-04-21 16:58:25 +0800

    samples 1

---
+++ dict/custom/1713687721784!1418029290429/detail.md
@@ -0,0 +1,1 @@
+![图片](810a19d8bc3eb1357e78d6deab1ea8d3fd1f4413.jpg)
---
+++ dict/custom/1713687721784!1418029290429/hook.js
@@ -0,0 +1,15 @@
+function validate(val, ctx) {
+
+}
+
+function translate(values, ctx) {
+
+}
+
+function adjust(entrys, ctx) {
+
+}
+
+function output(val, ctx) {
+
+}
